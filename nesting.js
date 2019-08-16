7e7dac6baf12ee0137cc42a2810b50faf5c96fc7
Brian Lowe <mynameisboy@gmail.com>
2019-08-16 17:30:08 -0600

    finished nesting

--- nesting.js
+++ nesting.js
@@ -9,37 +9,36 @@
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
-var employees = [
-  {
-    "firstName": "Von",
-    "lastName": "Budibent",
-    "email": "vbudibent0@163.com",
-    "department": "Sales"
-  },
-  {
-    "firstName": "Catherina",
-    "lastName": "Swalowe",
-    "email": "cswalowe1@example.com",
-    "department": "Engineering"
-  },
-  {
-    "firstName": "Theo",
-    "lastName": "Trill",
-    "email": "ttrill2@sina.com.cn",
-    "department": "Services"
-  },
-  {
-    "firstName": "Elsy",
-    "lastName": "McCrorie",
-    "email": "emccrorie3@netscape.com",
-    "department": "Legal"
-  },
-  {
-    "firstName": "Lorie",
-    "lastName": "Handsheart",
-    "email": "lhandsheart4@fotki.com",
-    "department": "Research and Development"
-  }
+var employees = [{
+        "firstName": "Von",
+        "lastName": "Budibent",
+        "email": "vbudibent0@163.com",
+        "department": "Sales"
+    },
+    {
+        "firstName": "Catherina",
+        "lastName": "Swalowe",
+        "email": "cswalowe1@example.com",
+        "department": "Engineering"
+    },
+    {
+        "firstName": "Theo",
+        "lastName": "Trill",
+        "email": "ttrill2@sina.com.cn",
+        "department": "Services"
+    },
+    {
+        "firstName": "Elsy",
+        "lastName": "McCrorie",
+        "email": "emccrorie3@netscape.com",
+        "department": "Legal"
+    },
+    {
+        "firstName": "Lorie",
+        "lastName": "Handsheart",
+        "email": "lhandsheart4@fotki.com",
+        "department": "Research and Development"
+    }
 ];
 // Do not edit the code above.
 
@@ -50,8 +49,19 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+function employeeUpdater() {
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].firstName === 'Theo') {
+            employees.splice(i, 1)
+        } else if (employees[i].firstName === 'Lorie') {
+            employees[i].department = 'HR'
+        }
+    }
+    console.log(employees)
+    return employees
+}
 
+employeeUpdater()
 
 
 ////////// PROBLEM 2 //////////
@@ -68,27 +78,34 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
-
+function removeDuplicates() {
+    for (let i = workplaceAccidents.length - 1; i >= 0; i--) {
+        for (let j = workplaceAccidents.length - 1; j >= 0; j--) {
+            if (workplaceAccidents[i] === workplaceAccidents[j] && i !== j) {
+                workplaceAccidents.splice(i, 1)
+            }
+        }
+    }
+    return workplaceAccidents
+}
 
 
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
 var cat = {
-  name: 'Fluffy',
-  catFriends: [
-    {
-      name: 'Grumpy',
-      activities: ['be grumpy', 'eat food']
-    }, 
-    {
-      name: 'Lazy Bones',
-      activities: ['sleep', 'pre-sleep naps']
+        name: 'Fluffy',
+        catFriends: [{
+                name: 'Grumpy',
+                activities: ['be grumpy', 'eat food']
+            },
+            {
+                name: 'Lazy Bones',
+                activities: ['sleep', 'pre-sleep naps']
+            }
+        ]
     }
-  ]
-}
-// Do not edit the code above.
+    // Do not edit the code above.
 
 /*
   Fluffy has two friends, Grumpy and Lazy Bones. 
@@ -97,8 +114,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -106,28 +123,27 @@ var fluffy2ndFriend;
 
 // Do not edit the code below.
 var myCar = {
-  make: 'Toyota',
-  model: 'Corolla',
-  year: 1992,
-  accidents: [
-    {
-      date: '3/15/93',
-      damage: '$5,000',
-      atFaultForAccident: true
-    },
-    {
-      date: '7/4/98',
-      damage: '$2,200',
-      atFaultForAccident: true
-    },
-    {
-      date: '6/22/99',
-      damage: '$7,900',
-      atFaultForAccident: true
+        make: 'Toyota',
+        model: 'Corolla',
+        year: 1992,
+        accidents: [{
+                date: '3/15/93',
+                damage: '$5,000',
+                atFaultForAccident: true
+            },
+            {
+                date: '7/4/98',
+                damage: '$2,200',
+                atFaultForAccident: true
+            },
+            {
+                date: '6/22/99',
+                damage: '$7,900',
+                atFaultForAccident: true
+            }
+        ]
     }
-  ]
-}
-// Do not edit the code above.
+    // Do not edit the code above.
 
 /*
   Above is some information about my car. As you can see, I am not the best driver.
@@ -138,14 +154,40 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+//first try
+
+// function recordCleaner() {
+//     for (let prop in myCar) {
+//         for (let i = 0; i < myCar.accidents.length; i++) {
+//             for (let prop2 in myCar.accidents[i])
+//                 if (myCar.accidents[i].atFaultForAccident) {
+//                     myCar.accidents[i].atFaultForAccident = false
+//                 }
+//         }
+//     }
+// }
+
+
+//second try
+
+function recordCleaner() {
+    for (let i = 0; i < myCar.accidents.length; i++) {
+        if (myCar.accidents[i].atFaultForAccident) {
+            myCar.accidents[i].atFaultForAccident = false
+        }
+    }
+}
 
 
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
+var numsArr = [
+    [1, 2, 3, 4],
+    [5, 6],
+    [7, 8, 9, 10, 11]
+];
 // Do not edit the code above.
 
 /*
@@ -157,6 +199,14 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
-
-
+function looper() {
+    for (let i = 0; i < numsArr.length; i++) {
+        for (let j = 0; j < numsArr[i].length; j++)
+            if (numsArr[i][j] % 2 === 0) {
+                numsArr[i][j] = 'even'
+            } else {
+                numsArr[i][j] = 'odd'
+            }
+    }
+    return numsArr
+}
